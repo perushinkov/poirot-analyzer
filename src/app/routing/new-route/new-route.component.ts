6a7287b4973f37a2d26ac29a65ecd31cd2517679
perushinkov <fantasmo3@gmail.com>
2019-06-02 08:23:00 +0300

    First part of UI flow: On persistence select, transition to import

--- src/app/routing/new-route/new-route.component.ts
+++ src/app/routing/new-route/new-route.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   templateUrl: './new-route.component.html',
@@ -14,13 +15,17 @@ export class NewRouteComponent implements OnInit {
       ],
     selected: -1
   };
-  constructor() {}
+  constructor(private route: ActivatedRoute, private router: Router) {}
 
   ngOnInit() {
   }
 
   selectPersistence(selectedIndex: number) {
     this.persistence.selected = selectedIndex;
+    // TODO:
+    //  await this.appState.setupPersistence;
+    this.router.navigate(['import']);
+
   }
 
 }
